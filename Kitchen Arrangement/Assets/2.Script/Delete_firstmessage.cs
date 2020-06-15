6c4e7fb7748f627f06bf13340bb1665bad245a6b
CHAEYOUNG <jade0.0v@users.noreply.github.com>
2020-06-15 06:40:18 +0200

    06_13

---
+++ Kitchen Arrangement/Assets/2.Script/Delete_firstmessage.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Delete_firstmessage : MonoBehaviour
+{
+
+	public float DestroyTime = 2.0f;
+	void Start()
+	{
+		Destroy(gameObject, DestroyTime);
+	}
+}
