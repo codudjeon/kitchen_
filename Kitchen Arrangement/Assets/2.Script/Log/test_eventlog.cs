d94b6cb42bab49bd18a9f16f039f36a21d9163dd
CHAEYOUNG <jade0.0v@users.noreply.github.com>
2020-07-23 14:36:41 +0200

    0723
    eventlog_test

---
+++ Kitchen Arrangement/Assets/2.Script/Log/test_eventlog.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using VRTK;
+
+public class test_eventlog : MonoBehaviour
+{
+    public void mytest(object o, ControllerInteractionEventArgs e)
+    {
+        print("hello");
+    }
+}
