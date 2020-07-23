83591f4949d92d67ec8e994011f6a9f326701128
CHAEYOUNG <jade0.0v@users.noreply.github.com>
2020-07-23 14:08:25 +0200

    0722

---
+++ Kitchen Arrangement/Assets/2.Script/Scenechange/Scenechangeend.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenechangeend : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+    private string End;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Door")
+    {
+            SceneManager.LoadScene("End");
+    }
+  }
+}
